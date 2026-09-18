8f179da7afaee9786c7cc644a3c5cc51c77a1e3c
Reema4r <faires000000@gmail.com>
2026-09-18 15:02:47 +0300

    Delete InkNote-v15 directory

--- InkNote-v15/dist/config.js
+++
@@ -1,9 +0,0 @@
-window.INKNOTE_CONFIG = {
-  adsenseClient: "",
-  adsenseSlot: "",
-  contactEmail: "",
-  siteUrl: "",
-  defaultRetentionHours: 24,
-  maxRetentionDays: 15,
-  maxFileSizeMB: 25
-};
