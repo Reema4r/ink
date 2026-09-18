cc5730e8356265f11210f08ffd1a5001c64349eb
Reema4r <faires000000@gmail.com>
2026-09-18 15:02:05 +0300

    Add files via upload

---
+++ inknote_v16/dist/config.js
@@ -0,0 +1,9 @@
+window.INKNOTE_CONFIG = {
+  adsenseClient: "",
+  adsenseSlot: "",
+  contactEmail: "",
+  siteUrl: "",
+  defaultRetentionHours: 24,
+  maxRetentionDays: 15,
+  maxFileSizeMB: 25
+};
